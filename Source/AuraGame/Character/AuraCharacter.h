73773454c63395b2d908ffc923bc365b65fd50e1
Paul Llamoja <palls257@gmail.com>
2025-06-23 22:20:35 -0500

    Add player character and enemy base character

---
+++ Source/AuraGame/Character/AuraCharacter.h
@@ -0,0 +1,23 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+UCLASS()
+class AURAGAME_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
---
+++ Source/AuraGame/Character/EnemyCharacter.h
@@ -0,0 +1,21 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/AuraCharacterBase.h"
+#include "EnemyCharacter.generated.h"
+
+UCLASS()
+class AURAGAME_API AEnemyCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
