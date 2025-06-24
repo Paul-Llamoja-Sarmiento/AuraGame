a925a04803ef723feeedd654ae6020c181023fc7
Paul Llamoja <palls257@gmail.com>
2025-06-24 12:47:26 -0500

    Setup enhanced input for player basic movement

---
+++ Source/AuraGame/Player/AuraPlayerController.cpp
@@ -0,0 +1,57 @@
+﻿
+#include "AuraPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = false;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ensure(IsValid(AuraMappingContext));
+
+	auto MappingSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	ensure(IsValid(MappingSubsystem));
+
+	MappingSubsystem->AddMappingContext(AuraMappingContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Crosshairs;
+
+	FInputModeGameAndUI InputMode;
+	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputMode.SetHideCursorDuringCapture(false);
+	SetInputMode(InputMode);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	auto EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(AuraMoveInputAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	APawn* ControlledPawn = GetPawn();
+	if (!IsValid(ControlledPawn))
+	{
+		return;
+	}
+
+	const FVector2D MoveValue = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = FRotator(0.0f, GetControlRotation().Yaw, 0.0f);
+	const FVector ForwardDirection = FRotationMatrix(Rotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(Rotation).GetUnitAxis(EAxis::Y);
+
+	ControlledPawn->AddMovementInput(ForwardDirection, MoveValue.Y);
+	ControlledPawn->AddMovementInput(RightDirection, MoveValue.X);
+}
---
+++ Source/AuraGame/Player/AuraPlayerController.h
@@ -0,0 +1,38 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+
+class UInputAction;
+class UInputMappingContext;
+
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAME_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	virtual void SetupInputComponent() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> AuraMoveInputAction;
+
+	void Move(const FInputActionValue& InputActionValue);
+};
