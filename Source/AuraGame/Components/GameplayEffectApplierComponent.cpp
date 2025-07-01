5fca38e75eabb97276da059c9d5f51dfee8862e4
Paul Llamoja <palls257@gmail.com>
2025-06-30 19:29:26 -0500

    Refactor EffectActor into a configurable component and add a FireArea with infinite duration as long as the character overlaps its area

---
+++ Source/AuraGame/Components/GameplayEffectApplierComponent.cpp
@@ -0,0 +1,123 @@
+﻿
+#include "GameplayEffectApplierComponent.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
+
+UGameplayEffectApplierComponent::UGameplayEffectApplierComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UGameplayEffectApplierComponent::OnBeginOverlapHandle(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                                           UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+                                                           bool bFromSweep, const FHitResult& SweepResult)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor);
+	if (!IsValid(TargetASC))
+	{
+		return;
+	}
+	
+	for (const auto& EffectConfig : EffectsToApply)
+	{
+		if (EffectConfig.ApplicationPolicy == EEffectApplicationPolicy::DoNotApply)
+		{
+			continue;
+		}
+
+		if (EffectConfig.ApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+		{
+			ApplyEffectToTarget(TargetASC, EffectConfig);
+		}
+	}
+
+	if (bShouldDestroyAfterApplyEffects)
+	{
+		GetOwner()->Destroy();
+	}
+}
+
+void UGameplayEffectApplierComponent::OnEndOverlapHandle(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                                         UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor);
+	if (!IsValid(TargetASC))
+	{
+		return;
+	}
+
+	for (const auto& EffectConfig : EffectsToApply)
+	{
+		if (EffectConfig.ApplicationPolicy == EEffectApplicationPolicy::DoNotApply)
+		{
+			continue;
+		}
+
+		if (EffectConfig.ApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+		{
+			ApplyEffectToTarget(TargetASC, EffectConfig);
+		}
+
+		if (EffectConfig.RemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+		{
+			RemoveEffectsFromTarget(TargetASC);
+		}
+	}
+
+	if (bShouldDestroyAfterApplyEffects)
+	{
+		GetOwner()->Destroy();
+	}
+}
+
+void UGameplayEffectApplierComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UPrimitiveComponent* CollisionVolume = GetOwner()->FindComponentByTag<UPrimitiveComponent>(CollisionVolumeTag);
+	check(CollisionVolume);
+	CollisionVolume->OnComponentBeginOverlap.AddDynamic(this, &UGameplayEffectApplierComponent::OnBeginOverlapHandle);
+	CollisionVolume->OnComponentEndOverlap.AddDynamic(this, &UGameplayEffectApplierComponent::OnEndOverlapHandle);
+}
+
+void UGameplayEffectApplierComponent::ApplyEffectToTarget(UAbilitySystemComponent* TargetASC, const FEffectApplicationConfig& EffectConfig)
+{
+	check(EffectConfig.GameplayEffect);
+
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(
+		EffectConfig.GameplayEffect, 1.0f, EffectContextHandle);
+
+	FActiveGameplayEffectHandle ActiveEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(
+		*EffectSpecHandle.Data);
+
+	const bool bHasInfiniteDuration = EffectSpecHandle.Data->Def->DurationPolicy ==
+		EGameplayEffectDurationType::Infinite;
+	if (bHasInfiniteDuration && EffectConfig.RemovalPolicy != EEffectRemovalPolicy::DoNotRemove)
+	{
+		ActiveEffectsMap.FindOrAdd(TargetASC).Add(ActiveEffectHandle);
+	}
+}
+
+void UGameplayEffectApplierComponent::RemoveEffectsFromTarget(UAbilitySystemComponent* TargetASC)
+{
+	if (!ActiveEffectsMap.Contains(TargetASC))
+	{
+		return;
+	}
+
+	for (const auto& ActiveEffectHandle : ActiveEffectsMap[TargetASC])
+	{
+		if (ActiveEffectHandle.IsValid())
+		{
+			TargetASC->RemoveActiveGameplayEffect(ActiveEffectHandle);
+		}
+	}
+
+	ActiveEffectsMap.Remove(TargetASC);
+}
+
---
+++ Source/AuraGame/Components/GameplayEffectApplierComponent.h
@@ -0,0 +1,83 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameplayEffectApplierComponent.generated.h"
+
+
+class UAbilitySystemComponent;
+class UGameplayEffect;
+struct FActiveGameplayEffectHandle;
+
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy : uint8
+{
+	DoNotApply     UMETA(DisplayName = "Don't Apply"),
+	ApplyOnOverlap UMETA(DisplayName = "Apply on Begin Overlap"),
+	ApplyOnEndOverlap UMETA(DisplayName = "Apply on End Overlap")
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy : uint8
+{
+	DoNotRemove      UMETA(DisplayName = "Don't Remove"),
+	RemoveOnEndOverlap UMETA(DisplayName = "Remove on End Overlap")
+};
+
+USTRUCT(BlueprintType)
+struct FEffectApplicationConfig
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS")
+	TSubclassOf<UGameplayEffect> GameplayEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS")
+	EEffectApplicationPolicy ApplicationPolicy = EEffectApplicationPolicy::ApplyOnOverlap;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS",
+		meta = (EditCondition = "ApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap", EditConditionHides,
+			ToolTip = "Only relevant if the effect has an infinite duration. Ignored otherwise."))
+	EEffectRemovalPolicy RemovalPolicy = EEffectRemovalPolicy::DoNotRemove;
+};
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AURAGAME_API UGameplayEffectApplierComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UGameplayEffectApplierComponent();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Applied Effects")
+	TArray<FEffectApplicationConfig> EffectsToApply;
+
+	// TODO: Consider using a weak pointer if a stale pointer arises
+	TMap<UAbilitySystemComponent*, TArray<FActiveGameplayEffectHandle>> ActiveEffectsMap;
+
+	UPROPERTY(EditAnywhere, Category = "Applied Effects",
+		meta = (ToolTip =
+			"Must tag the collision volume component of the owner actor with this name. It won't work otherwise."))
+	FName CollisionVolumeTag = "effect-applier-collision-volume";
+
+	UPROPERTY(EditAnywhere, Category = "Applied Effects")
+	bool bShouldDestroyAfterApplyEffects = true;
+
+	UFUNCTION()
+	void OnBeginOverlapHandle(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	                          UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                          const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnEndOverlapHandle(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	                        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	virtual void BeginPlay() override;
+
+	void ApplyEffectToTarget(UAbilitySystemComponent* TargetASC, const FEffectApplicationConfig& EffectConfig);
+
+	void RemoveEffectsFromTarget(UAbilitySystemComponent* TargetASC);
+};
