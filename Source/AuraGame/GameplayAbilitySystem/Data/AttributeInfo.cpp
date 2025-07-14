5f4e657e0526bef75f4eac3e8f8a26e69867aecc
Paul Llamoja <palls257@gmail.com>
2025-07-14 15:55:51 -0500

    Create the Data Asset Attribute Info, to set and fetch data related to the attributes we will be using in the Attribute Menu

---
+++ Source/AuraGame/GameplayAbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,13 @@
+﻿
+#include "AttributeInfo.h"
+
+bool UAttributeInfo::FindAttributeInfoByTag(const FGameplayTag& AttributeTag, FAttributeInfoStruct& InAttributeInfo) const
+{
+	if (AttributeInfoMap.Contains(AttributeTag))
+	{
+		InAttributeInfo = AttributeInfoMap[AttributeTag];
+		return true;
+	}
+
+	return false;
+}
---
+++ Source/AuraGame/GameplayAbilitySystem/Data/AttributeInfo.h
@@ -0,0 +1,37 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AttributeInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAttributeInfoStruct
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attribute Info")
+	FText AttributeName = FText();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attribute Info")
+	FText AttributeDescription = FText();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute Info")
+	float AttributeValue = -1.f;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAME_API UAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	bool FindAttributeInfoByTag(const FGameplayTag& AttributeTag, FAttributeInfoStruct& InAttributeInfo) const;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attribute Info")
+	TMap<FGameplayTag, FAttributeInfoStruct> AttributeInfoMap;
+};
